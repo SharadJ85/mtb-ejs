6357654376f9b8f33cb4dff229d2df653c933631
Sharad Jadhav <sharad.jadhav@wafer.ee>
2020-03-09 00:03:08 +0530

    top_rated, now_playing end points set and .env configured

--- src/api.ts
+++ src/api.ts
@@ -1,24 +1,70 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 /**
- * TMDB contains the default url for 'The Movie DataBase' site
+ * TURL contains the default API url for 'The Movie DataBase' site
  * @URLmethods perfixLink + movieId + apikey
  * @method baseURL: default URL
+ * @method mediatype: type:- | movie | tv_show | person
+ * @method generalFeatures: type:- | popular | now_playing | top_rated
  * @method movieID: movie ID number
  * @method apikey: user API ky to access the TMDB database
+ * @method page: generalFeatures(0) page number
  */
 
-export class TMDB<T> {
+export class Turl<T> {
+  /**
+   * returns baseURL=https://api.themoviedb.org/3/
+   */
   baseURL() {
-    const baseUrl="https://api.themoviedb.org/3/movie/";
-    return baseUrl
+    return "https://api.themoviedb.org/3/";
   }
-  movieId(id: number) {
-    return id
+
+  /**
+   * returns type
+   * @param type | 0: movie | 1: tv | 2: person
+   */
+  mediatype(type: number) {
+    const types: string[] = ["movie/", "tv/", "person/"];
+    for (const index in types) {
+      if (parseInt(index) === type) {
+        return types[index];
+      }
+    }
   }
+
+  /**
+   * returns type
+   * @param type | 0: popular | 1: now_playing | 2: top_rated
+   */
+  generalFeatures(type: number) {
+    const types: string[] = ["popular/", "now_playing/", "top_rated/"];
+    for (const index in types) {
+      if (parseInt(index) === type) {
+        return types[index];
+      }
+    }
+  }
+
+  /**
+   * @param id type() ID number
+   */
+  Id(id: number) {
+    return id;
+  }
+
+  /**
+   * returns the user API_KEY stored in ".env" file
+   */
   apikey() {
-    const key: string = "d531f0b35e33ab3572f10065361d3ae1";
-    const apiky="?api_key=" + key
-    return apiky;
+    const key: any = process.env.API_KEY;
+    return "?api_key=" + key;
+  }
+
+  /**
+   * @param page generalFeatures(0) page number
+   */
+  page(page: number) {
+    return "&"+page;
   }
 }
-
-// https://api.themoviedb.org/3/movie/550?api_key=d531f0b35e33ab3572f10065361d3ae1
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
+import dotenv from "dotenv";
 import express, { Response, Request } from "express";
 import fetch from "node-fetch";
 import path from "path";
-import { TMDB } from "./api";
+import {Turl} from "./api";
+
+// config dotenv and set its path
+dotenv.config({ path: ".." });
 
 const app = express();
 // tslint:disable-next-line: radix
-const port: number = parseInt(process.env.NODE_ENV as string) || 5000;
+const port: number = parseInt(process.env.PORT as string) || 5050;
 
 // Configure Express to use EJS
 app.set("views", path.join(__dirname, "client/views"));
@@ -20,9 +24,10 @@ app.listen(port, () => {
   console.log("");
   console.log(`=========================================`);
   console.log(`=========================================`);
+  // tslint:disable-next-line: radix
 });
 
-const tmdb = new TMDB();
+const turl = new Turl();
 
 // define a route handler for the default home page
 app.get("/", (req: Request, res: Response, next) => {
@@ -31,9 +36,24 @@ app.get("/", (req: Request, res: Response, next) => {
 });
 
 app.get("/popular", async (req: Request, res: Response, next) => {
-  // http://api.themoviedb.org/3/movie/popular?api_key=d531f0b35e33ab3572f10065361d3ae1
-  const popularMovies = tmdb.baseURL() + "popular" + tmdb.apikey();
+  // http://api.themoviedb.org/3/movie/popular?api_key=API_KEY
+  const popularMovies: string =
+    turl.baseURL() + turl.mediatype(0) + turl.generalFeatures(0) + turl.apikey()
   const fetchData = await fetch(popularMovies);
-  const data = await fetchData.json();
+  const data: Response = await fetchData.json();
   res.json(data);
+});
+
+app.get("/now_playing", async (req: Request, res: Response, next) => {
+  const nowPlaying: string =
+    turl.baseURL() + turl.mediatype(0) + turl.generalFeatures(1) + turl.apikey();
+  const fetchData = await fetch(nowPlaying);
+  res.json(await fetchData.json());
+});
+
+app.get("/top_rated", async (req: Request, res: Response, next) => {
+  const nowPlaying: string =
+    turl.baseURL() + turl.mediatype(0) + turl.generalFeatures(2) + turl.apikey();
+  const fetchData = await fetch(nowPlaying);
+  res.json(await fetchData.json());
 });
